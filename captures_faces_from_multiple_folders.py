65a85945f7b4af1edf3285af02b9fca0bfed6a73
Santiago Benavidez <115203597+TITIR1X@users.noreply.github.com>
2022-12-18 04:31:25 -0300

    Update captures_faces_from_multiple_folders.py

--- captures_faces_from_multiple_folders.py
+++ captures_faces_from_multiple_folders.py
@@ -1,9 +1,12 @@
-import cv2, os
+import cv2
+import platform, os
 
-try:
- os.system('color 6')
- os.system('cls')
-except:os.system('clear')
+os_name = platform.system()
+
+if os_name == 'Windows':
+    os.system('cls')
+elif os_name == "Linux":
+    os.system('clear')
  
 print("""
  ./capture_face_from_images.py
